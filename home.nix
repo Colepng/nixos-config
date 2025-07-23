9ef593304b817c090cf9ffb9fb9b7d4de80e3f91
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2025-07-23 12:12:48 -0400

    Inital Nixos Config

---
+++ home.nix
@@ -0,0 +1,51 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "cole";
+  home.homeDirectory = "/home/cole";
+
+  home.stateVersion = "25.05";
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+	xournalpp
+	neofetch
+	ripgrep
+	btop
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+  };
+
+  home.sessionVariables = {
+    # EDITOR = "neovim";
+  };
+
+  services = {
+    syncthing = {
+      enable = true;
+      tray.enable = true;
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Cole Kauder-McMurrich";
+    userEmail = "gamingwithcole7@gmail.com";
+  };
+  
+  programs.firefox.enable = true;
+  
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+  };
+}

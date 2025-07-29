60687d6de8e03b47fcc912438b3d44aedbfb4584
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2025-07-28 20:59:42 -0400

    Enable steam

--- configuration.nix
+++ configuration.nix
@@ -113,7 +113,7 @@
   programs.zsh.enable = true;
 
   programs.steam = {
-    enable = false;
+    enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers

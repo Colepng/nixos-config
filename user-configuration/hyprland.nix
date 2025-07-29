c90b836a70fb887b0e104c6b236123db6573e863
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2025-07-29 12:37:01 -0400

    Add some base window rules

--- user-configuration/hyprland.nix
+++ user-configuration/hyprland.nix
@@ -93,6 +93,14 @@
 
       gestures.workspace_swipe = true;
 
+      windowrule = [
+      	# Ignore maximize requests from apps. You'll probably like this.
+	"suppressevent maximize, class:.*"
+
+	# Fix some dragging issues with XWayland
+	"nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+      ];
+
       "$mainMod" = "SUPER";
       "$altMod" = "ALT";
       "$altMainMod" = "SUPER + SHIFT";

25b4b9bd08285362faca6c51effb60e41ebc548a
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2025-09-09 16:59:45 -0400

    Change power governors

--- system-configuration/laptop.nix
+++ system-configuration/laptop.nix
@@ -10,23 +10,23 @@
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
 
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 20;
+      CPU_MAX_PERF_ON_BAT = 50;
 
       # Optional helps save long term battery health
-      START_CHARGE_THRESH_BAT1 = 40; # 40 and bellow it starts to charge
-      STOP_CHARGE_THRESH_BAT1 = 80; # 80 and above it stops charging
+      # START_CHARGE_THRESH_BAT1 = 40; # 40 and bellow it starts to charge
+      # STOP_CHARGE_THRESH_BAT1 = 80; # 80 and above it stops charging
 
       MEM_SLEEP_ON_AC = "s2idle";
       MEM_SLEEP_ON_BAT = "s2idle";
 
       CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 0;
+      CPU_BOOST_ON_BAT = 1;
     };
   };
 

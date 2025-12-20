cdcccf25c3372508d671e399714870649e6871a6
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2025-12-20 13:24:15 -0500

    Install obs

--- home.nix
+++ home.nix
@@ -46,6 +46,25 @@
     hunspellDicts.en_CA
     gnome-software
   ];
+  programs.obs-studio = {
+    enable = true;
+
+    # optional Nvidia hardware acceleration
+    package = (
+      pkgs.obs-studio.override {
+        cudaSupport = true;
+      }
+    );
+
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi # optional AMD hardware acceleration
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
 
   programs = {
     direnv = {

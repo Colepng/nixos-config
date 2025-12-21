384c2f823086152bc37ac6b9564b5755a1180312
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2025-12-20 23:19:49 -0500

    Auto mount game drive

--- machine/lenovo/configuration.nix
+++ machine/lenovo/configuration.nix
@@ -4,5 +4,16 @@
 }:
 
 {
+
+  fileSystems."/mnt/Games" = {
+    device = "/dev/disk/by-uuid/a8efed91-0f5a-46e7-973b-b5704daf4afe";
+    fsType = "btrfs";
+    options = [
+      "users"
+      "nofail"
+      "x-gvfs-show"
+    ];
+  };
+
   networking.hostName = "lenovo";
 }

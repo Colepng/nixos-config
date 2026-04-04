7069fa444e4f878042c70d838d834d2d82437f62
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2026-04-03 21:21:28 -0400

    Touchscreen config stuff

--- machine/framework/configuration.nix
+++ machine/framework/configuration.nix
@@ -1,8 +1,17 @@
 {
   config,
+  lib,
+  pkgs,
   ...
 }:
 
 {
+  hardware.bluetooth.enable = true;
+  # Enable OpenTabletDriver
+  hardware.opentabletdriver.enable = true;
+
+  # Required by OpenTabletDriver
+  hardware.uinput.enable = true;
+  boot.kernelModules = [ "uinput" ];
   networking.hostName = "framework";
 }

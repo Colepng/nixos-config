1c58a9d759514d4f5dce6df880cbb32ec7bca547
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2026-05-16 19:09:05 -0400

    Disable open table driver

--- machine/framework/configuration.nix
+++ machine/framework/configuration.nix
@@ -7,11 +7,6 @@
 
 {
   hardware.bluetooth.enable = true;
-  # Enable OpenTabletDriver
-  hardware.opentabletdriver.enable = true;
 
-  # Required by OpenTabletDriver
-  hardware.uinput.enable = true;
-  boot.kernelModules = [ "uinput" ];
   networking.hostName = "framework";
 }

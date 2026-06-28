713fc33c06296288c5ce79d6357b6b1c7d7a552d
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2026-06-28 12:48:55 -0400

    Setup jellyfin

--- machine/pc/services.nix
+++ machine/pc/services.nix
@@ -6,6 +6,15 @@
       virtualHosts."photos.colepng.com".extraConfig = ''
         reverse_proxy localhost:2283
       '';
+
+      virtualHosts."jellyfin.colepng.com".extraConfig = ''
+        reverse_proxy localhost:8096
+      '';
+    };
+
+    jellyfin = {
+      enable = true;
+      openFirewall = true;
     };
   };
 }

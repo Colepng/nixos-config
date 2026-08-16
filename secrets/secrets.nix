f81540ccdda7ce2bd63658a68b80340a8ddf5921
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2026-08-16 12:50:33 -0400

    Add framework key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,7 @@
 let
   pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBotTIkimcU+7Kt0ZavDzt6dH1PsTb38zjbcN5TFc39a";
+
+  framework = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAb4sxQtDQK3a5rIYNFIep6XwzjXIqqVzXRnBM6hRm07";
 in
 {
   "restic.age".publicKeys = [ pc ];

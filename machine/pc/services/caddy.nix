30ebabd875888e8391e4e510b10700dabca62d8c
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2026-08-16 11:42:45 -0400

    Break services up

---
+++ machine/pc/services/caddy.nix
@@ -0,0 +1,22 @@
+{ ... }:
+{
+  services.caddy = {
+    enable = true;
+
+    virtualHosts."photos.colepng.com".extraConfig = ''
+      reverse_proxy localhost:2283
+    '';
+
+    virtualHosts."immich.colepng.com".extraConfig = ''
+      reverse_proxy localhost:2283
+    '';
+
+    virtualHosts."jellyfin.colepng.com".extraConfig = ''
+      reverse_proxy localhost:8096
+    '';
+
+    virtualHosts."dawarich.colepng.com".extraConfig = ''
+      reverse_proxy localhost:3000
+    '';
+  };
+}
